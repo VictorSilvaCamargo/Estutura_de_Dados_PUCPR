389aafe45e6524c1a06b7d18067e92f2a8319e30
Victor S Camargo <107776635+VictorSilvaCamargo@users.noreply.github.com>
2023-08-09 20:30:10 -0300

    Delete Aula04 directory

--- Aula04/Main.java
+++
@@ -1,8 +0,0 @@
-package Aula04;
-
-public class Main {
-    public static void main(String[] args) {
-        Carro carro1 = new Carro("1234","Vermelho","sedan","flex",4);
-        carro1.exibirDados();
-    }
-}
