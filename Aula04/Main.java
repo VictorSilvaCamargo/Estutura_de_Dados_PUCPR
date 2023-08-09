3a3e9a686a812de921e1b9cfdb9fe1e80d7bcc46
Victor S. Camargo <victorscamargo51@gmail.com>
2023-08-09 20:03:22 -0300

    Herança

---
+++ Aula04/Main.java
@@ -0,0 +1,8 @@
+package Aula04;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("1234","Vermelho","sedan","flex",4);
+        carro1.exibirDados();
+    }
+}
