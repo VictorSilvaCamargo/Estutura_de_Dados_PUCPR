cd4323e5e5adb49053a571dcbaa30b42e482f48e
Victor S. Camargo <victorscamargo51@gmail.com>
2023-08-21 08:14:02 -0300

    Moda

--- Aula03/Moda.java
+++ Aula03/Moda.java
@@ -1,5 +1,6 @@
 package Aula03;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Moda {
@@ -8,6 +9,7 @@ public class Moda {
         int modaAtual = 1;
         int numeroModa = lista[0];
         int contador = 1;
+        ArrayList<Integer>numerosModa = new ArrayList<>();
 
         for(int i = 0; i < lista.length;i++){
             for(int j = i + 1; j < lista.length;j++){
@@ -18,23 +20,24 @@ public class Moda {
                 if(contador > modaAtual){
                     numeroModa = lista[i];
                     modaAtual = contador;
+                    numerosModa.add(numeroModa);
+
                 }
             }
 
         }
+
         if(modaAtual == 1){
             System.out.println("Não à nenhum número que se repete mais de uma vez");
 
         } else {
             System.out.println(numeroModa);
         }
-
-
     }
 
     public static void main(String[] args) {
 
-        int[] numeros = {2,3,4,5,6,3};
+        int[] numeros = {2,3,4,5,6,3,4};
         System.out.println(Arrays.toString(numeros));
         moda(numeros);
 
