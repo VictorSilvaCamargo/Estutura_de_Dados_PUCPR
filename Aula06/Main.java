1e5bca81788be9dff12e6f5da978edacd3907f17
Victor S. Camargo <victorscamargo51@gmail.com>
2023-08-28 07:48:04 -0300

    Lista Encadeada

--- Aula06/Main.java
+++ Aula06/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         ListaEncadeada listaEncadeada = new ListaEncadeada();
 
-        int choice = 0;
+        int choice;
 
         do {
             System.out.println("Menu:");
@@ -20,14 +20,13 @@ public class Main {
             choice = scanner.nextInt();
 
             switch (choice) {
-                case 1:
+                case 1 -> {
                     System.out.print("Digite o elemento a ser inserido: ");
                     int elementToAdd = scanner.nextInt();
                     listaEncadeada.add(elementToAdd);
                     System.out.println("Elemento inserido.");
-                    break;
-
-                case 2:
+                }
+                case 2 -> {
                     System.out.print("Digite o elemento a ser procurado: ");
                     int elementToSearch = scanner.nextInt();
                     Node foundNode = listaEncadeada.procurar(elementToSearch);
@@ -36,27 +35,19 @@ public class Main {
                     } else {
                         System.out.println("Elemento não encontrado.");
                     }
-                    break;
-
-                case 3:
+                }
+                case 3 -> {
                     System.out.print("Digite o elemento a ser removido: ");
                     int elementToRemove = scanner.nextInt();
                     listaEncadeada.remove(elementToRemove);
                     System.out.println("Elemento removido.");
-                    break;
-
-                case 4:
+                }
+                case 4 -> {
                     System.out.println("Conteúdo da lista:");
                     listaEncadeada.imprimir();
-                    break;
-
-                case 5:
-                    System.out.println("Saindo do programa.");
-                    break;
-
-                default:
-                    System.out.println("Opção inválida.");
-                    break;
+                }
+                case 5 -> System.out.println("Saindo do programa.");
+                default -> System.out.println("Opção inválida.");
             }
 
         } while (choice != 5);
